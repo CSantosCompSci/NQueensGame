ef89ea36a73998f28e3f87dd05752db2870c01b0
OnTheRiseCompSci <csantos0822@gmail.com>
2015-09-19 15:57:57 -0400

    Working on it

--- Tester/src/Main.java
+++ Tester/src/Main.java
@@ -33,83 +33,92 @@ public class Main {
 	public static int solve(int numOfQueens)
 	{
 		int numOfSolutions = 0;
-		int filled = 0;
 		int columnNumber = 0;
 		boolean noMoreSolutions =false;
 		solutionStack.push(columnNumber);
-		System.out.println(solutionStack.toString());
-		while(columnNumber < numOfQueens)
+		while(!noMoreSolutions)
 			{
-			
-				if(solutionStack.size() == numOfQueens  )//filled == numOfQueens)
-				{
-					System.out.println("Exit 2");
+				System.out.println("stack = " + solutionStack.size());
+				System.out.println();
 
-					numOfSolutions++;
-					printBoard(solutionStack);
-					//noMoreSolutions = true;
-				}else
-				if (solutionSpace(solutionStack, columnNumber) == true)
+				for(int j = 0; j < solutionStack.size(); j++)
 				{
-					System.out.println("Exit 3");
-					solutionStack.push(columnNumber);
-					System.out.println(solutionStack.toString());
-					System.out.println(solutionStack.size());
-					System.out.println("colNum = " +columnNumber);
+					System.out.println("For loop");
+					System.out.println("j = " + j);
+					System.out.println("stack " + solutionStack.toString());
+					System.out.println("size " + solutionStack.size());
+					System.out.println("col = " +columnNumber);
+					System.out.println();
 
-					filled++;
 					
-				}else
-				if(solutionSpace(solutionStack, columnNumber) == false)
-				{
-					System.out.println("Exit 4");
+					if(solutionStack.get(j) == columnNumber)
+					{
+				
+						System.out.println("1st if");
+					System.out.println("j = " +	solutionStack.get(j));
+				
+					System.out.println("stack " + solutionStack.toString());
+					System.out.println("size " + solutionStack.size());
+					System.out.println("col = " + columnNumber);
 					columnNumber++;
-					System.out.println("colNum = " +columnNumber);
-					System.out.println(solutionStack.peek());
-					solutionStack.push(solutionStack.pop()+1);
-					System.out.println("Stack after pop " + solutionStack.toString());
+					System.out.println();
+					break;
 
-					filled--;
+					}
+					 
+					if(Math.abs(solutionStack.get(j) - columnNumber) == Math.abs(j - solutionStack.size()))
+					{	
+					System.out.println("get j = " + solutionStack.get(j));
+					System.out.println("col = " + columnNumber);
+					System.out.println("j =" + j);
+					System.out.println("size = " + solutionStack.size());
+					System.out.println("rowdif = " + Math.abs(solutionStack.get(j) - columnNumber));
+					System.out.println("colDif = " + Math.abs(j - solutionStack.size()));
+					System.out.println("2nd if");
+					System.out.println("stack" + solutionStack.toString());
+					System.out.println("size " + solutionStack.size());
+					System.out.println("col = " +columnNumber);
+					columnNumber++;
+					break;
+					}
+					 if(columnNumber == numOfQueens)
+					{
+						columnNumber = solutionStack.pop() ;
 					
-				}
-			
-		}
-		System.out.println("Exit 1");
-		
-		return numOfSolutions;
-	}
-
-	// Boolean solution that returns true if space is a valid anwser. False for when it is not a solution space.
-
-	public static boolean solutionSpace(Stack<Integer> workingStack, int columnNumber)
-	{
-		
-			Boolean isSolution  = false;
-			int rowNumber  = workingStack.peek();
-			for( int j = 0; j < workingStack.size(); j++)
-			{	
-				if(workingStack.get(j) == columnNumber)
-				{
-					System.out.println("j " + j);
-					System.out.println("colNum = " +columnNumber);
-					System.out.println(workingStack.get(j));
-					isSolution =  false;
-				
-				}
-				else if(Math.abs(workingStack.get(j)- j ) == Math.abs(columnNumber -j))
-				{
-					System.out.println(Math.abs(rowNumber- columnNumber) + "hello"+Math.abs(rowNumber - solutionStack.size()) );
-					isSolution = false;
+					System.out.println("3rd if");
+					System.out.println("stack = " + solutionStack.toString());
+					System.out.println("size" + solutionStack.size());
+					System.out.println("col = " +columnNumber);
+					System.out.println();
+					}
+					if(solutionStack.size() == numOfQueens)
+					{
+						numOfSolutions++;
+						printBoard(solutionStack);
+					    columnNumber = solutionStack.pop() + 1 ;
+						System.exit(1);
+						
+					}
+					if(solutionStack.isEmpty() && columnNumber == numOfQueens)
+					 	noMoreSolutions = true;
+					if(solutionStack.get(j) != columnNumber && Math.abs(solutionStack.get(j) - columnNumber) != Math.abs(j - solutionStack.size()))
+					{
 					
-				}
-				else
-				{
-					isSolution = true;
-				
+						System.out.println("else");
+						System.out.println("stack " +solutionStack.toString());
+						System.out.println("size " +solutionStack.size());
+						System.out.println(columnNumber);
+						System.out.println();
+
+						solutionStack.push(columnNumber);
+						columnNumber = 0;
+						break;
+					}
+					else 
+					columnNumber++;
 				}
 			}
-			System.out.println(isSolution);
-			return isSolution;
+			return numOfSolutions;
 	}
 	/* Using the solution stack this method prints the board using a Q to represent the queen and a * to represent the 
 	 * an empty square
